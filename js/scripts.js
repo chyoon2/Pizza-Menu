2b9986d10539165e5d8eb96f0690008823e402b5
Chris Yoon <chy.yoon@gmail.com>
2020-06-19 14:24:08 -0700

    Revert back to older commit, to avoid bugs that were created.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,32 @@
-
 function Pizza() {
   this.pizzaOrder = [];
-  this.price = 0;
 }
 
-Pizza.prototype.addInfo= function(pie) {
-  this.pizzaOrder.push(pie);
+Pizza.prototype.addInfo= function(pizza, pie) {
+  pizza.pizzaOrder.push(pie);
 }
 
 Pizza.prototype.cost = function (pizza, pie) {
 
-  // pizza.pizzaOrder.forEach(function(element){
-    for(index=0; index <= 2; index++) {
+  pizza.pizzaOrder.forEach(function(element){
     let pieSize = pizza.pizzaOrder[0].size;
-    // let numOfTopping = pizza.pizzaOrder[0].pie[topping]["length"];
-    // let = costOfSize = 0;
-    // console.log(pieSize)
-    // if (pieSize === "small") {
-    //   costOfSize = 10;
-    // } else if (pieSize === "medium"){
-    //   costOfSize = 12;
-    // } else if (pieSize === "large"){
-    //   costOfSize = 14;
-    // } else {
-    //   costOfSize = 16;
-    // }
-    // let totalCost = costOfSize + parseInt(numOfTopping)
+    let numOfTopping = pizza.pizzaOrder[0].topping["length"];
+    let = costOfSize= 0;
     
-    // let sum = pizza.price + totalCost;
-    // console.log(sum)
-  }
-  // });
+    if (pieSize === "small") {
+      costOfSize = 10;
+    } else if (pieSize === "medium"){
+      costOfSize = 12;
+    } else if (pieSize === "large"){
+      costOfSize = 14;
+    } else {
+      costOfSize = 16;
+    }
+    let totalCost = costOfSize + parseInt(numOfTopping)
+    
+    console.log(totalCost)
+    // totalCost.push
+  });
 
   // totalCost = sizeCost + (numOftopping * 2)
 }
@@ -40,20 +36,16 @@ function Pie(size, topping) {
   this.topping = topping; 
 }
 
-let pizza = new Pizza();
-
-
 
 // User interface
 $(document).ready(function() {
   $("form#form-one").submit(function(event) {
     event.preventDefault();
-
+    let pizza = new Pizza();
     const inputSize = $("#size").val();
     let inputTopping =$("input:checkbox[name=topping]:checked")
     let pie = new Pie(inputSize, inputTopping);
     pizza.addInfo(pizza, pie);
     pizza.cost(pizza, pie);
-    // displayCost();
   });
 });
