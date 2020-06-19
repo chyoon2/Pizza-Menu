1ce6f81f0e28513aa8c290b4b73f2476bb5d2db3
Chris Yoon <chy.yoon@gmail.com>
2020-06-19 14:39:44 -0700

    Add a function for displaying the price

--- js/scripts.js
+++ js/scripts.js
@@ -7,37 +7,36 @@ Pizza.prototype.addInfo= function(pizza, pie) {
 }
 
 Pizza.prototype.cost = function (pizza, pie) {
-
-  pizza.pizzaOrder.forEach(function(element){
-    let pieSize = pizza.pizzaOrder[0].size;
-    let numOfTopping = pizza.pizzaOrder[0].topping["length"];
-    let = costOfSize= 0;
-    
-    if (pieSize === "small") {
-      costOfSize = 10;
-    } else if (pieSize === "medium"){
-      costOfSize = 12;
-    } else if (pieSize === "large"){
-      costOfSize = 14;
-    } else {
-      costOfSize = 16;
-    }
-    let totalCost = costOfSize + parseInt(numOfTopping)
-    
-    console.log(totalCost)
-    // totalCost.push
-  });
-
-  // totalCost = sizeCost + (numOftopping * 2)
+  // pizza.pizzaOrder.forEach(function(){
+  let pieSize = pizza.pizzaOrder[0].size;
+  let numOfTopping = pizza.pizzaOrder[0].topping["length"];
+  let = costOfSize= 0;
+  
+  if (pieSize === "small") {
+    costOfSize = 10;
+  } else if (pieSize === "medium"){
+    costOfSize = 12;
+  } else if (pieSize === "large"){
+    costOfSize = 14;
+  } else {
+    costOfSize = 16;
+  }
+  let totalCost = costOfSize + parseInt(numOfTopping)
+  console.log(totalCost)
+  return totalCost
 }
-
 function Pie(size, topping) {
   this.size = size;
   this.topping = topping; 
 }
-
-
 // User interface
+
+function displayCost (totalCost) {
+  let displayPrice = $("#outcome");
+  $("#outcome").show();
+  (displayPrice).text(`$totalCost`);
+}
+
 $(document).ready(function() {
   $("form#form-one").submit(function(event) {
     event.preventDefault();
@@ -46,6 +45,7 @@ $(document).ready(function() {
     let inputTopping =$("input:checkbox[name=topping]:checked")
     let pie = new Pie(inputSize, inputTopping);
     pizza.addInfo(pizza, pie);
-    pizza.cost(pizza, pie);
+    totalCost =pizza.cost(pizza, pie);
+    displayCost(totalCost);
   });
 });
