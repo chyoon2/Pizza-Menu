84527b9c3a393ff01c154ccb4962f3169892ea12
Chris Yoon <chy.yoon@gmail.com>
2020-06-19 08:55:45 -0700

    Add Object constructors Pizza, Pie, and prototypes addInfo and cost

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+function Pizza() {
+  this.pizzaOrder = [];
+}
+
+Pizza.prototype.addInfo(pizza, size, topping) {
+  pizzaOrder.push(pie);
+}
+
+function Pie(size, topping) {
+  this.size = size;
+  this.topping = topping; 
+}
+
+Pizza.prototype.cost = function () {
+  
+}
+
+
+let pizza = new Pizza();
+let pie = new Pie();
+$(document).ready(function() {
+
+});
