1c984f076d2fe72c53127d67ffc921af02a97f1b
Chris Yoon <chy.yoon@gmail.com>
2020-06-19 09:27:27 -0700

    Add formula and variables to the cost prototype function

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ function Pizza() {
   this.pizzaOrder = [];
 }
 
-Pizza.prototype.addInfo(pizza, size, topping) {
+Pizza.prototype.addInfo= function(pie) {
   pizzaOrder.push(pie);
 }
 
@@ -11,13 +11,26 @@ function Pie(size, topping) {
   this.topping = topping; 
 }
 
-Pizza.prototype.cost = function () {
-  
+Pizza.prototype.cost = function (pizza) {
+  let small = 10;
+  let medium = 13;
+  let large = 16;
+  let xlarge = 19;
+
+  let topping = 1;
+
+  pizzaOrder.forEach(function(numOfPizza){
+    Pizza.pizzaOrder[numOfPizza].forEach(function(desc) {
+      sizeCost = Pizza.pizzaOrder.[numOfPizza].size[desc];
+      console.log(sizeCost)
+      toppingCost = Pizza.pizzaOrder.[numOfPizza].topping[desc];
+    });
+    
+  });
 }
 
-
-let pizza = new Pizza();
-let pie = new Pie();
 $(document).ready(function() {
+  let pizza = new Pizza();
+  let pie = new Pie();
 
 });
