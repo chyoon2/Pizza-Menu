63cd517f065bb87b044e6f5f2302665edc9d7fd7
Chris Yoon <chy.yoon@gmail.com>
2020-06-19 13:57:07 -0700

    fixed bugs and removed foreach and for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,36 @@
+
 function Pizza() {
   this.pizzaOrder = [];
+  this.price = 0;
 }
 
-Pizza.prototype.addInfo= function(pizza, pie) {
-  pizza.pizzaOrder.push(pie);
+Pizza.prototype.addInfo= function(pie) {
+  this.pizzaOrder.push(pie);
 }
 
 Pizza.prototype.cost = function (pizza, pie) {
 
-  pizza.pizzaOrder.forEach(function(element){
+  // pizza.pizzaOrder.forEach(function(element){
+    for(index=0; index <= 2; index++) {
     let pieSize = pizza.pizzaOrder[0].size;
-    let numOfTopping = pizza.pizzaOrder[0].topping["length"];
-    let = costOfSize= 0;
+    // let numOfTopping = pizza.pizzaOrder[0].pie[topping]["length"];
+    // let = costOfSize = 0;
+    // console.log(pieSize)
+    // if (pieSize === "small") {
+    //   costOfSize = 10;
+    // } else if (pieSize === "medium"){
+    //   costOfSize = 12;
+    // } else if (pieSize === "large"){
+    //   costOfSize = 14;
+    // } else {
+    //   costOfSize = 16;
+    // }
+    // let totalCost = costOfSize + parseInt(numOfTopping)
     
-    if (pieSize === "small") {
-      costOfSize = 10;
-    } else if (pieSize === "medium"){
-      costOfSize = 12;
-    } else if (pieSize === "large"){
-      costOfSize = 14;
-    } else {
-      costOfSize = 16;
-    }
-    let totalCost = costOfSize + parseInt(numOfTopping)
-    
-    console.log(totalCost)
-    // totalCost.push
-  });
+    // let sum = pizza.price + totalCost;
+    // console.log(sum)
+  }
+  // });
 
   // totalCost = sizeCost + (numOftopping * 2)
 }
@@ -36,16 +40,20 @@ function Pie(size, topping) {
   this.topping = topping; 
 }
 
+let pizza = new Pizza();
+
+
 
 // User interface
 $(document).ready(function() {
   $("form#form-one").submit(function(event) {
     event.preventDefault();
-    let pizza = new Pizza();
+
     const inputSize = $("#size").val();
     let inputTopping =$("input:checkbox[name=topping]:checked")
     let pie = new Pie(inputSize, inputTopping);
     pizza.addInfo(pizza, pie);
     pizza.cost(pizza, pie);
+    // displayCost();
   });
 });
